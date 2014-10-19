53741f7ff6d6f7e96dda0ef9f891eb61c0e09673
thbourlove <thbourlove@gmail.com>
2014-10-20 00:37:55 +0800

    move vi-mode and add gopath

--- config.fish
+++ config.fish
@@ -1,3 +1,5 @@
-set fish_path $HOME/.config/fish
+set -x FISH_PATH $HOME/.config/fish
+set -x GOPATH $HOME/Develop/go
+set -x PATH $PATH $GOPATH/bin
 
-. $fish_path/functions/vi-mode.fish
+. $HOME/.config/fish/vi-mode.fish
