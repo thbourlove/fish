9ff255e2b9e53bba1e94ee90a23b2fae7f2083f5
thbourlove <thbourlove@gmail.com>
2014-10-23 13:19:24 +0800

    add vi mode in prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -57,6 +57,8 @@ set __bobthefish_dk_grey    333
 set __bobthefish_med_grey   999
 set __bobthefish_lt_grey    ccc
 
+set __bobthefish_yellow b58900
+
 # ===========================
 # Helper methods
 # ===========================
@@ -256,6 +258,38 @@ function __bobthefish_prompt_dir -d 'Display a shortened form of the current dir
   __bobthefish_path_segment "$PWD"
 end
 
+function __bobthefish_vi_mode -d 'Display vi mode'
+  switch $vi_mode
+    case $vi_mode_insert
+      set_color -b $__bobthefish_yellow
+      set_color 000
+      echo -n I
+    case $vi_mode_normal
+      set_color -b $__bobthefish_lt_grey
+      set_color 000
+      echo -n N
+    case $vi_mode_g
+      set_color -b $__bobthefish_med_green
+      set_color 000
+      echo -n G
+    case $vi_mode_swapcase
+      set_color -b $__bobthefish_med_green
+      set_color 000
+      echo -n S
+    case $vi_mode_upper
+      set_color -b $__bobthefish_dk_green
+      set_color 000
+      echo -n U
+    case $vi_mode_lower
+      set_color -b $__bobthefish_lt_green
+      set_color 000
+      echo -n L
+    case $vi_mode_replace $vi_mode_REPLACE
+      set_color -b $__bobthefish_med_red
+      set_color 000
+      echo -n R
+  end
+end
 
 # ===========================
 # Apply theme
@@ -263,6 +297,7 @@ end
 
 function fish_prompt -d 'bobthefish, a fish theme optimized for awesome'
   set -g RETVAL $status
+  __bobthefish_vi_mode
   __bobthefish_prompt_status
   __bobthefish_prompt_user
   if __bobthefish_in_git
