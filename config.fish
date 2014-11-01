cdeb748bba5dc29d1afae50bcad0bd7a432b530c
thbourlove <thbourlove@gmail.com>
2014-11-02 03:18:06 +0800

    add /usr/local/sbin and new function with_proxy

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 set -x FISH_PATH $HOME/.config/fish
 set -x GOPATH $HOME/Develop/go
+set -x PATH /usr/local/sbin $PATH
 set -x PATH $PATH $GOPATH/bin
 
 . $HOME/.config/fish/vi-mode.fish
---
+++ functions/with_proxy.fish
@@ -0,0 +1,3 @@
+function with_proxy
+    env http_proxy=http://z.elema.com:1984 https_proxy=http://z.elema.com:1984 $argv
+end
