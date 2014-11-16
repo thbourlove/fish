df9d5ca53e9021b82885b3ff26000934b5d44eb8
thbourlove <thbourlove@gmail.com>
2014-11-16 11:20:45 +0800

    set locale env

--- config.fish
+++ config.fish
@@ -1,3 +1,7 @@
+set -x LANG en_US.UTF-8
+set -x LANGUAGE en_US.UTF-8
+set -x LC_TYPE en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
 set -x FISH_PATH $HOME/.config/fish
 set -x GOPATH $HOME/Develop/go
 set -x PATH /usr/local/sbin $PATH
