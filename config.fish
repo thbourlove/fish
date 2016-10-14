e7bb5409fb1defb07a9d2ea555900453b8d6059d
thbourlove <thbourlove@gmail.com>
2016-10-14 10:07:49 +0800

    check go command before set GOPATH and GOROOT

--- config.fish
+++ config.fish
@@ -3,7 +3,13 @@ set -x LANGUAGE en_US.UTF-8
 set -x LC_TYPE en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x FISH_PATH $HOME/.config/fish
-set -x GOPATH $HOME/Develop/go
-set -x GOROOT /usr/local/opt/go/libexec
 set -x PATH /usr/local/sbin $PATH
-set -x PATH $PATH $GOPATH/bin
+if type go >/dev/null 2>&1
+    if test -d $HOME/Develop/go
+        set -x GOPATH $HOME/Develop/go
+        set -x PATH $PATH $GOPATH/bin
+    end
+    if test -d /usr/local/opt/go/libexec
+        set -x GOROOT /usr/local/opt/go/libexec
+    end
+end
